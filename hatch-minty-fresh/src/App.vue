aea558af5f4b4a2311074079889d5651a0c6ba44
Mitchell Hall <mitchell.hall400@gmail.com>
2023-02-08 23:43:51 -0600

    Added wireframe

--- hatch-minty-fresh/src/App.vue
+++ hatch-minty-fresh/src/App.vue
@@ -1,60 +1,65 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-app-bar class="px-3" color="white" app>
+      <v-avatar color="grey darken-1" size="32"></v-avatar>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-tabs centered color="grey darken-2">
+        <v-tab v-for="link in links" :key="link">
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
     </v-app-bar>
 
-    <v-main>
-      <HelloWorld/>
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-sheet rounded="xl" min-height="268">
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" sm="8">
+            <v-sheet rounded="xl">
+              <v-sparkline
+                :value="value"
+                :gradient="gradient"
+                :smooth="radius || false"
+                :padding="padding"
+                :line-width="width"
+                :stroke-linecap="lineCap"
+                :gradient-direction="gradientDirection"
+                :fill="fill"
+                :type="type"
+                :auto-line-width="autoLineWidth"
+              ></v-sparkline>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
+  name: "App",
   data: () => ({
-    //
+    links: ["Home", "Settings"],
+    width: 2,
+    radius: 10,
+    padding: 10,
+    lineCap: 'round',
+    gradient: ['red', 'orange', 'yellow'],
+    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+    labels: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+    gradientDirection: 'top',
+    fill: true,
+    type: 'trend',
+    autoLineWidth: false,
   }),
 };
 </script>
