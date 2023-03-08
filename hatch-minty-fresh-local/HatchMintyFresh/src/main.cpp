6ed65593d11e5f8f9a4f8ed7571b246d04334fb8
matt-hall0100 <matt.hall0100@gmail.com>
2023-03-08 17:32:32 -0600

    Added abilty to read the two DHT22 Sensors

--- hatch-minty-fresh-local/HatchMintyFresh/src/main.cpp
+++ hatch-minty-fresh-local/HatchMintyFresh/src/main.cpp
@@ -1,9 +1,37 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+// define GPIO
+#define LOWERDHTPIN 14  // pin D5
+#define UPPERDHTPIN 12  // pin D6
+
+// Create DHT Objects
+DHT updht(UPPERDHTPIN, DHT22);
+DHT lowdht(LOWERDHTPIN, DHT22);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // DHT setup
+  updht.begin();
+  lowdht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float h = lowdht.readHumidity();
+  float f = lowdht.readTemperature(true);
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(f);
+  Serial.print(F("°F "));
+  h = updht.readHumidity();
+  f = updht.readTemperature(true);
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(f);
+  Serial.print(F("°F \n\n"));
+  delay(2000);
 }
