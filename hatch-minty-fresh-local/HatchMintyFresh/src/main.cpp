1ac4ce0c350d991714a1281538f01125dfd079fb
matt-hall0100 <matt.hall0100@gmail.com>
2023-03-08 21:55:17 -0600

    Connected Output of PID to Light Dimmer

--- hatch-minty-fresh-local/HatchMintyFresh/src/main.cpp
+++ hatch-minty-fresh-local/HatchMintyFresh/src/main.cpp
@@ -6,6 +6,8 @@
 #include <PID_v1.h>
 
 // define GPIO
+#define ZCROSS 5        // pin D1
+#define PWM 4           // pin D2
 #define LOWERDHTPIN 14  // pin D5
 #define UPPERDHTPIN 12  // pin D6
 
@@ -17,6 +19,11 @@ DHT lowdht(LOWERDHTPIN, DHT22);
 double Setpoint, Input, Output;
 double Kp = 2, Ki = 5, Kd = 1;
 PID tempPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+// timer vars
+unsigned long dimmerStartTime = millis();
+unsigned long pidStartTime = millis();
+int counter = 0;
 
 /*------------------------------------------------- getAvgTemp -------
 |  Function getAvgTemp
@@ -42,16 +49,26 @@ double getAvgTemp() {
 |  Returns:  none
 *-------------------------------------------------------------------*/
 void updatePID() {
+  pidStartTime = millis();
   Input = getAvgTemp();
-  tempPID.Compute();
-  Serial.print(F("\nPID Output Updated to: "));
-  Serial.print(Output);
+  if(Input == Input) {  // check for NaN
+    tempPID.Compute();
+  }
 }
 
+IRAM_ATTR void ZeroCross() {
+  dimmerStartTime = millis();
+  digitalWrite(PWM, LOW);
+}
 
 void setup() {
   Serial.begin(115200);
 
+  // dimmer setup
+  pinMode(PWM, OUTPUT);
+  pinMode(ZCROSS, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ZCROSS), ZeroCross, RISING);
+  
   // DHT setup
   updht.begin();
   lowdht.begin();
@@ -60,12 +77,24 @@ void setup() {
   Setpoint = 99.5;
   Input = getAvgTemp();
   tempPID.SetOutputLimits(0, 100);
+  tempPID.SetSampleTime(1000);
   tempPID.SetMode(AUTOMATIC);
 }
 
+
 void loop() {
-  Serial.print(F("\nTemperature: "));
-  Serial.print(getAvgTemp());
-  updatePID();
-  delay(2000);
+  if (counter == 10){
+    Serial.print(F("\n\nPID Output set: "));
+    Serial.print(Output);
+    Serial.print(F("\nTemperature: "));
+    Serial.print(Input);
+    counter = 0;
+  }
+  if(millis() - pidStartTime >= 1000) {
+    updatePID();
+    counter++;
+  }
+  if(millis() - dimmerStartTime >= (((100-Output)/100.0)*8.33)) {
+    digitalWrite(PWM, HIGH);
+  }
 }
